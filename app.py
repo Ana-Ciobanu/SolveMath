cfc7d8784b4245b3ad4382320e48680be5adc3d9
Ana Ciobanu <Ana.Ciobanu@endava.com>
2025-07-22 09:37:42 +0300

    monitoring

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from fastapi import FastAPI
 from controllers.controllers import router
 from db.database import engine
 from models.models import Base
+from prometheus_fastapi_instrumentator import Instrumentator
+
+def setup_monitoring(app):
+    Instrumentator().instrument(app).expose(app)
 
 app = FastAPI(title="Math Operations API", version="1.0")
 app.include_router(router)
+
+setup_monitoring(app)
 
 # Create tables
 Base.metadata.create_all(bind=engine)
